db4ec5ef0f74ba8c3113e3d7f0c3d528b5d0c6fb
yakornyushenko <9yar9@mail.ru>
2023-01-15 01:21:48 +0100

    Frontend: pages registration, terms of service, authorisation

---
+++ client/src/pages/Terms.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import {Button, Container} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {REGISTRATION_ROUTE} from "../utils/consts";
+
+const Terms = () => {
+        //TODO alive checkbox
+    return (
+        <Container>
+                <h2 className=' d-flex justify-content-center mt-4 mb-2'>Terms of service</h2>
+           <div className='border border-primary p-2'>
+                   Lorem ipsum dolor sit amet,
+                   consectetur adipisicing elit.
+                   Alias, aspernatur, cupiditate
+                   doloribus ducimus, expedita iste modi natus non
+                   obcaecati odit quisquam soluta vitae. Ad animi ipsam, omnis quaerat vel voluptates.
+                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque autem distinctio non quas.
+                   Accusantium corporis nostrum quibusdam. Accusantium aliquam animi atque deleniti dignissimos dolorem dolorum
+                   eum ex explicabo impedit iste laboriosam nemo nihil, perspiciatis quisquam repellat sunt voluptate? Aliquid
+                   corporis dicta doloremque dolores dolorum eaque facilis quam sed totam unde. Aperiam aut error esse eveniet
+                   ipsum vel! Aliquam asperiores dolor dolores facilis hic officiis sapiente veritatis. Adipisci asperiores
+                   atque
+                   aut autem commodi consectetur culpa deleniti dolor eaque eos expedita explicabo facere illum impedit
+                   incidunt
+                   ipsa iusto labore laborum laudantium maiores nihil nostrum odit perferendis quae quibusdam quidem quis,
+                   repell
+                   endus reprehenderit saepe similique tenetur voluptate voluptates voluptatum. Adipisci blanditiis
+                   consequuntur
+                   dolores eius eligendi error ex id labore laborum modi necessitatibus neque obcaecati odio provident quaerat
+                   quisquam, quod rem, repellat sed similique sit, voluptas voluptate voluptatum. Beatae corporis et iusto
+                   minus
+                   odit qui quibusdam quis sint. Consectetur corporis error facere facilis iste maxime provident quaerat?
+                   Aperiam
+                   officia, voluptates? Ab aliquam asperiores aspernatur atque cum dolores error eum, eveniet explicabo fugiat
+                   impedit
+                   laborum maiores necessitatibus non nostrum nulla obcaecati odio omnis porro quasi quis quo ratione sapiente
+                   sed
+                   totam unde vero? Amet aperiam aut culpa, excepturi, magnam, maiores minima modi numquam possimus quasi
+                   similique
+                   tempore. Adipisci aliquid deleniti doloribus eius enim facilis fugit, ipsa minus necessitatibus nisi, nobis
+                   praesentium quod, soluta suscipit tenetur? Accusantium aliquam doloremque dolores esse libero nesciunt offic
+                   iis quia repellendus reprehenderit tempora voluptatem, voluptates. Accusantium adipisci aliquid aperiam at a
+                   utem consequatur, cupiditate debitis dignissimos distinctio earum eius eum fugit illo, ipsa libero necessita
+                   tibus numquam officia porro possimus praesentium quisquam reiciendis rem reprehenderit sint sit tenetur
+                   velit
+                   . Accusantium aliquam consequatur delectus eos excepturi fugiat numquam quia recusandae repellat veritatis!
+                   Adipisci aperiam architecto asperiores assumenda autem corporis deserunt eos error fuga hic libero maxime
+                   natus
+                   neque odit, possimus quae quisquam reiciendis saepe sed sequi sint soluta suscipit totam ut velit vero
+                   voluptatum?
+                   Aperiam corporis dignissimos in laudantium minus numquam officia quae quos, recusandae ut. Adipisci aliquid
+                   amet at,
+                   atque autem consectetur cumque dolore dolorem doloremque doloribus dolorum ea est facilis fugit harum
+                   inventore ipsa
+                   minima necessitatibus numquam obcaecati quae quaerat repudiandae sapiente sunt velit. Accusamus atque
+                   deserunt harum, labore molestias nobis odio placeat provident repellendus totam? Amet distinctio enim error
+                   incidunt minus nam necessitatibus neque non, odio repudiandae. Accusantium aperiam commodi cum dolores ea
+                   enim eos error exercitationem illo labore minus molestias mollitia, nesciunt nostrum officia! Accusamus
+                   aliquam amet aperiam, asperiores consequatur corporis cum deleniti dolorum, illo nam porro quis
+                   reprehenderit ullam velit voluptas! Beatae consequatur delectus distinctio dolore doloribus eveniet ex illum
+                   inventore ipsa laborum laudantium magnam minima necessitatibus nostrum nulla obcaecati perferendis placeat,
+                   quidem rem repellat repudiandae rerum saepe sunt totam voluptas voluptatibus voluptatum? Alias, molestiae,
+                   nesciunt! Commodi dolore dolorum eaque error hic impedit incidunt ipsum necessitatibus, odio, quo repellat
+                   soluta, vero. Aspernatur blanditiis consectetur dolor dolore dolorum ea et eum fugiat in ipsa iste itaque
+                   labore maxime molestiae nobis possimus quam quis, saepe sapiente tempora. Commodi, mollitia voluptas. Ad
+                   adipisci aut beatae, dolor eligendi error, excepturi harum labore libero magnam modi necessitatibus neque
+                   omnis placeat quae quia quidem, quo recusandae rem similique sint sit voluptas. Dolor harum laudantium nihil
+                   sint soluta suscipit, veniam voluptas. A amet beatae blanditiis consequuntur dolorum ea eligendi explicabo
+                   facere illo, incidunt laudantium nemo neque nesciunt nihil non obcaecati officiis pariatur possimus quidem
+                   quisquam quos recusandae sapiente sunt suscipit tempore totam, unde ut, veritatis vero voluptates! Dolore
+                   est explicabo laborum magnam modi molestias nam nemo nulla odio officiis porro quae rem repudiandae, sed
+                   temporibus, tenetur voluptates. Consequatur deserunt eius esse fugit nemo nisi omnis perspiciatis quaerat
+                   repellendus similique. Adipisci aliquid amet commodi consequuntur dolorum esse est ex illo inventore ipsa
+                   ipsum maxime, necessitatibus nesciunt nihil nisi non numquam, obcaecati officia quaerat repellendus,
+                   sapiente ut velit veniam veritatis voluptate. Accusamus adipisci alias architecto asperiores aspernatur at
+                   atque consectetur cumque dicta dignissimos dolore et hic in inventore ipsa, iste itaque laborum magni
+                   maiores minima molestiae nam necessitatibus nesciunt odit omnis pariatur possimus quae quidem ratione
+                   recusandae sapiente sequi similique sunt tempora temporibus vel vitae. Accusantium ad amet commodi cumque
+                   debitis, dolores excepturi harum iste, molestiae omnis perspiciatis porro possimus quaerat qui quidem? Dolor
+                   dolores eaque fugit laboriosam molestias, ratione! Accusamus adipisci autem cum debitis dicta, dolor
+                   doloremque, enim est et fugiat fugit iusto magnam nisi obcaecati quae quia, rerum. Architecto cupiditate
+                   debitis, doloribus eligendi expedita ipsam natus odit perspiciatis rem saepe, sequi ullam! Doloribus
+                   exercitationem nostrum, quibusdam saepe sed sint voluptate. Dolorem, doloremque dolorum ex fugit impedit
+                   ipsa non ratione sint ullam vitae? Accusamus deleniti expedita id incidunt nulla obcaecati, quis quod sequi
+                   unde voluptas! Aliquid cupiditate deserunt, doloremque, ea earum eligendi esse et ex fugiat iure labore modi
+                   nobis officiis pariatur quisquam rem reprehenderit sapiente temporibus unde voluptatibus. Aliquid aut,
+                   beatae consequatur cupiditate distinctio enim eos eveniet expedita itaque nisi numquam odio odit officia
+                   praesentium quaerat quisquam rem similique veritatis? Ad amet aspernatur autem commodi consectetur, deleniti
+                   dignissimos ea, earum enim et eum explicabo ipsum minima molestiae molestias nam nihil numquam officia porro
+                   quae quam quia quisquam reiciendis reprehenderit tempore vel voluptas voluptate. Ab accusantium alias
+                   aspernatur commodi deleniti deserunt dolor dolorem doloremque eligendi error, esse eveniet fugit, harum id
+                   ipsa ipsam itaque iure iusto magni minima modi necessitatibus nostrum numquam odit omnis placeat porro quis
+                   quos ratione recusandae reiciendis sequi soluta ullam ut veniam vitae voluptatem! Aperiam obcaecati quia
+                   recusandae repellat? Aut blanditiis cumque debitis deserunt dicta dolore dolores, doloribus eos esse est
+                   exercitationem facere, fuga inventore magni minima mollitia natus necessitatibus nemo nihil nulla, officia
+                   omnis perferendis placeat porro quam quas quibusdam reprehenderit saepe sequi sit sunt tenetur totam unde ut
+                   veniam veritatis vero. Adipisci cupiditate et ex illum vitae voluptas. Ab amet, architecto at atque autem
+                   beatae, corporis dicta dignissimos dolore dolores et eveniet exercitationem harum id itaque libero maxime
+                   necessitatibus nesciunt nihil nostrum odit officiis provident reiciendis rem reprehenderit saepe ut. At,
+                   officiis praesentium? Architecto atque ipsa omnis reiciendis. Hic quis quisquam veniam
+                   vitae voluptatem! Ab architecto eius eum fuga id in, nulla praesentium quae quibusdam, quos recusandae
+                   sequi,
+                   similique? Reiciendis?
+                 <div className='d-flex justify-content-center'>
+                         <Link TO={REGISTRATION_ROUTE}>
+                         <Button className='m-2' style={{width: 200}} variant={'outline-primary'}>
+                                 I agree
+                         </Button>
+                         </Link>
+                         <Link to={REGISTRATION_ROUTE}>
+                                 <Button className='m-2' style={{width: 200}} variant={'outline-danger'}>
+                                         I disagree
+                                 </Button>
+                         </Link>
+
+                 </div>
+           </div>
+        </Container>
+    );
+};
+
+export default Terms;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,18 @@
 import Admin from "./pages/Admin";
-import {ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "./utils/consts";
+import {
+    ADMIN_ROUTE,
+    BASKET_ROUTE,
+    DEVICE_ROUTE,
+    LOGIN_ROUTE,
+    REGISTRATION_ROUTE,
+    SHOP_ROUTE,
+    TERMS_ROUTE
+} from "./utils/consts";
 import Basket from "./pages/Basket";
 import Shop from "./pages/Shop";
 import Auth from "./pages/Auth";
 import DevicePage from "./pages/DevicePage";
+import Terms from "./pages/Terms";
 
 export const authRoutes = [
     {
@@ -20,6 +29,10 @@ export const publicRoutes = [
     {
         path: SHOP_ROUTE,
         component: Shop
+    },
+    {
+        path: TERMS_ROUTE,
+        component: Terms
     },
     {
         path: LOGIN_ROUTE,
