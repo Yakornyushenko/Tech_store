178b3ad801fc3b4bc64502013dc71e2a3146b331
yakornyushenko <9yar9@mail.ru>
2023-01-25 21:54:13 +0100

    Connection to back: log out function, add loader, check auth user

---
+++ client/src/components/Loader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/component_style/loader.css';
+
+const Loader = () => {
+    return (
+        <div className='lds-spinner_wrap'>
+            <div className="lds-spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
