9f180e79d275457d4ca8b5e569d35403208ffd38
yakornyushenko <9yar9@mail.ru>
2023-01-29 15:21:46 +0100

    Connection to back

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, {useContext, useEffect, useState} from "react";
+import {BrowserRouter} from 'react-router-dom'
+import AppRouter from "./components/AppRouter";
+import {NavBar} from "./components/NavBar";
+import Footer from "./components/Footer";
+import {observer} from "mobx-react-lite";
+import {Context} from "./index";
+import {check} from "./http/userAuth";
+import Loader from "./components/Loader";
+
+const App = observer(() => {
+    const {user} = useContext(Context)
+    const [loadingPage, setLoadingPage] = useState(true)
+    useEffect(() => {
+        setTimeout(() => {
+            check().then(res => {
+                user.setUser(res)
+                user.setIsAuth(true)
+            }).finally(() => setLoadingPage(false))
+            console.log('user', user)
+        }, 1000)
+    }, [])
+
+    if (loadingPage) {
+        return <Loader/>
+    }
+    return (
+        <BrowserRouter>
+            <NavBar/>
+            <AppRouter/>
+            <Footer/>
+        </BrowserRouter>
+    );
+})
+
+export default App;
