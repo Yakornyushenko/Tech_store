31504d664809c343c0866e83e83744d0c532a5c4
Yaraslau <9yar9@mail.ru>
2023-01-30 18:01:58 +0000

    Revert "Merge branch 'Connection_to_back4' into 'main'"
    This reverts merge request !13

--- client/src/http/deviceCreate.js
+++ client/src/http/deviceCreate.js
@@ -1,16 +1,8 @@
 import {authHost, host} from "./index";
+import jwt_decode from "jwt-decode";
 
 export const createType = async (type) => {
     const {data} = await authHost.post('api/type', type)
-    return data
-}
-
-export const createBrand = async (brand) => {
-    const {data} = await authHost.post('api/brand', brand)
-    return data
-}
-export const createDevice = async (device) => {
-    const {data} = await authHost.post('api/device', device)
     return data
 }
 
